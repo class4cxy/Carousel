52e9b936007f023c5a8106fd9059140c37939999
chen <class4cxy@126.com>
2014-06-25 18:59:05 +0800

    create carousel.js

---
+++ carousel.js
@@ -0,0 +1,14 @@
+/**
+ * by jdo
+ * 20140625
+ * summary : a slider depend on zetpo, which work for mobile
+ */
+define([
+	'../zepto/zetpo'
+], function() {
+
+	// if exist
+	if ( $([]).carousel ) return;
+
+	
+});
